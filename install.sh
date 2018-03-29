af2eb00290742492d470cf7263947d3d3030813b
Tamas Kozma <kozma.tamas@ipsystems.hu>
2018-03-29 11:19:04 +0200

    Update Vundle vim git url, checkout directory and plugin install command

--- install.sh
+++ install.sh
@@ -35,11 +35,11 @@ function install_vim {
     echo    # (optional) move to a new line
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
-        vundle_dir="${HOME}/.vim/bundle/vundle.vim"
+        vundle_dir="${HOME}/.vim/bundle/Vundle.vim"
         if [ ! -d "$vundle_dir" ]; then
-            git clone https://github.com/vundlevim/vundle.vim.git "$vundle_dir"
+            git clone https://github.com/VundleVim/Vundle.vim.git "$vundle_dir"
         fi
-        vim +plugininstall +qall
+        vim +PluginInstall +qall
     fi
 }
 
