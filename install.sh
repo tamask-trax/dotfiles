e3db5b38338a19a2baf3091140611a36a66bc8c1
Tamas Kozma <tkozma88@gmail.com>
2019-02-14 14:38:38 +0100

    Update fonts settings
    - Remove fonts.conf - not needed
    - Add Xresources file
    - Update install.sh

--- install.sh
+++ install.sh
@@ -3,28 +3,27 @@
 set -euo pipefail
 
 dotfiles_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-olddir=~/dotfiles_old
-files="bashrc vimrc fonts.conf tmux.conf toprc"    # list of files/folders to symlink in homedir
+olddir="${HOME}/.dotfiles-old"
+
+files="bashrc vimrc tmux.conf toprc Xresources"    # list of files/folders to symlink in homedir
 
 echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
+mkdir -vp $olddir
 
-# change to the dotfiles directory
-echo "Changing to the $dotfiles_dir directory"
-cd $dotfiles_dir
-echo "...done"
-echo
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 echo "Moving any existing dotfiles from ~ to $olddir"
 for file in $files; do
-    if [ -f "${HOME}/.${file}" ] && [ ! -f "${HOME}/dotfiles_old/.${file}" ]; then
-        echo "Moving ${HOME}/.${file} to ~/dotfiles_old/"
-        mv "${HOME}/.${file}" ~/dotfiles_old/
+    if [ -f "${HOME}/.${file}" ] && [ ! -f "$olddir/.${file}" ]; then
+        mv -v "${HOME}/.${file}" "$olddir/"
     fi
+done
+echo "...done"
+echo
+
+# Create symlinks
+echo "Creating symlinks for dotfiles"
+for file in $files; do
     if [ ! -L "${HOME}/.${file}" ]; then
-        echo "Creating symlink to $file in home directory."
-        ln -s "$dotfiles_dir/$file" "${HOME}/.${file}"
+        ln -vs "$dotfiles_dir/$file" "${HOME}/.${file}"
     fi
 done
 echo "...done"
@@ -32,7 +31,7 @@ echo "...done"
 function install_vim {
     echo
     read -p "install vim plugins now [Yy]?" -n 1 -r
-    echo    # (optional) move to a new line
+    echo
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
         vundle_dir="${HOME}/.vim/bundle/Vundle.vim"
@@ -46,7 +45,7 @@ function install_vim {
 function install_tpm {
     echo
     read -p "install tmux plugins now [Yy]?" -n 1 -r
-    echo    # (optional) move to a new line
+    echo
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
         tpm_dir="${HOME}/.tmux/plugins/tpm"
